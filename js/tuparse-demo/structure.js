c5347dfd772de65cdb5729bcd8ae0ae5581adc86
Matthew Maxwell <m@matthewcmaxwell.com>
2014-01-02 15:46:50 -0600

    Add labels and expand handle section

--- js/tuparse-demo/structure.js
+++ js/tuparse-demo/structure.js
@@ -2,25 +2,29 @@ define({
   dom: {
     ".container.tuparse-demo-container": {
       ".row": {
-        ".col-lg-6.col-lg-offset-3": {
+        ".col-lg-6.col-lg-offset-3.col-md-6.col-md-offset-3": {
           ".header": {
             "h2.title": {
               _text: "Tuparse Shakur Demo"
             }
           }
         },
-        ".col-lg-6.col-lg-offset-3.tps-item-1": {
-          "form.custom-handler": {
-            "h4.title": {
-              "a": {
-                _text: "Custom Handles Module",
-                events: {
-                  click: {
-                    mid: "tuparse-demo/handles",
-                    method: "toggleCustomHandlesModule"
+        ".col-lg-12.col-md-12.custom-handler-container": {
+          ".custom-handler": {
+            ".row": {
+              ".col-lg-6.col-lg-offset-3.col-md-6.col-md-offset-3": {
+                "h4.title": {
+                  "a": {
+                    _text: "Custom Handles Module",
+                    events: {
+                      click: {
+                        mid: "tuparse-demo/handles",
+                        method: "toggleCustomHandlesModule"
+                      }
+                    },
+                    "span.caret": {}
                   }
-                },
-                "span.caret": {}
+                }
               }
             },
             "div.hidden.custom-module": {
@@ -74,7 +78,10 @@ define({
             }
           }
         },
-        ".col-lg-4.left-bar": {
+        ".col-lg-4.col-md-4.left-bar": {
+          "h5.title.text-muted": {
+            _text: "Source"
+          },
           "textarea.form-control.raw-code": {
             id: "rawCode",
             events: {
@@ -105,9 +112,14 @@ define({
             }
           }
         },
-        ".col-lg-8.content": {
-          ".parsed-code.tuparse-test-container": {
-            id: "parsedCode"
+        ".col-lg-8.col-md-8.content": {
+          ".parsed-code-container": {
+            "h5.title.text-muted": {
+              _text: "Render"
+            },
+            ".parsed-code.tuparse-test-container": {
+              id: "parsedCode"
+            }
           }
         }
       }
@@ -115,41 +127,62 @@ define({
   },
   css: {
     variables: {
-      "@black": "#333"
+      "@black": "#333",
+      "@gray": "#aaa"
     },
     styles: {
       ".tuparse-demo-container": {
         padding_bottom: "25px",
         ".left-bar": {
-          border_right: "1px solid @black",
+          border: "1px solid @gray",
+          padding_bottom: "15px",
+          padding_top: "15px",
+          ".title": {
+            margin_top: 0
+          },
           ".raw-code": {
-            height: "800px",
+            height: "750px",
             margin_bottom: "15px"
           }
         },
-        ".parsed-code": {
-          width: "100%"
-        },
-        ".custom-handler": {
-          ".title": {
-            "a": {
-              cursor: "pointer",
-              text_decoration: "none",
-              ".caret": {
-                margin_left: "5px",
-                "&.caret-up": {
-                  border_bottom: "4px solid",
-                  border_top: 0
-                },
-              }
+        ".content": {
+          padding_right: 0,
+          ".parsed-code-container": {
+            padding: "15px",
+            border: "1px solid @gray",
+            ".title": {
+              margin_top: 0
+            },
+            ".parsed-code": {
+              width: "100%"
             }
-          },
-          ".custom-module": {
-            border: "1px solid @black",
-            margin_bottom: "10px",
-            padding: "10px",
-            ".module-definition": {
-              height: "500px"
+          }
+        },
+        ".custom-handler-container": {
+          padding_left: 0,
+          padding_right: 0,
+          ".custom-handler": {
+            ".title": {
+              padding_left: "10px",
+              "a": {
+                cursor: "pointer",
+                text_decoration: "none",
+                ".caret": {
+                  margin_left: "5px",
+                  "&.caret-up": {
+                    border_bottom: "4px solid",
+                    border_top: 0
+                  },
+                }
+              }
+            },
+            ".custom-module": {
+              border: "1px solid @gray",
+              margin_bottom: "25px",
+              padding: "10px",
+              ".module-definition": {
+                height: "500px"
+              }
             }
           }
         }
